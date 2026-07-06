a9dd4cc7e120325b0b0bc964bdcf39e14bb3c58b
주진현 <joojinhyun00@gmail.com>
2026-07-06 17:23:04 +0900

    feat: export public API surface

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,5 @@
-export {};
+export { createClient } from "./client.js";
+export type { CreateClientOptions, DataGoKrClient, Params } from "./client.js";
+export { DataGoKrError } from "./errors.js";
+export type { NormalizedResult } from "./errors.js";
+export type { OperationResult, RawItem, RawApiResponse } from "./envelope.js";
